bb2af4c93850aff9d8b35d4831e19cb416a957a3
Waqar Bin Abrar <waqaremadina1@gmail.com>
2024-12-18 15:52:46 +0500

    updated more

--- connection/connection.js
+++ connection/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-const mongoURI = "mongodb+srv://waqaremadina1:uNw3ZdCrLrSBqDHz@cluster0.2fh3n.mongodb.net/";
+const mongoURI = process.env.MONGO_CONNECTION_STRING;
 
 const connectToMongo = async () => {
   try {
